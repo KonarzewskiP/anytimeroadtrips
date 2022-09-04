7b31b9e0031634713ccee14d4409fb589692e0e5
KonarzewskiP <pawelkonarzewski13@gmail.com>
2022-09-04 09:14:49 +0100

    initial commit

---
+++ src/main/java/com/anytimeroadtrips/AnytimeRoadtripsApplication.java
@@ -0,0 +1,13 @@
+package com.anytimeroadtrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnytimeRoadtripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnytimeRoadtripsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anytimeroadtrips/AnytimeRoadtripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anytimeroadtrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnytimeRoadtripsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
