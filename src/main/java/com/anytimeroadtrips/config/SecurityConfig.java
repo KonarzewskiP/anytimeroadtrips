3e7a6d65423ee22e82e14b60f329fb8430523e6f
KonarzewskiP <pawelkonarzewski13@gmail.com>
2022-10-22 13:52:17 +0100

    Add initial Spring security configuration

--- src/main/java/com/anytimeroadtrips/config/SecurityConfig.java
+++ src/main/java/com/anytimeroadtrips/config/SecurityConfig.java
@@ -1,8 +1,23 @@
 package com.anytimeroadtrips.config;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http){
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(withDefaults())
+                .authorizeExchange(auth -> {
+                    auth.anyExchange().permitAll();
+                })
+                .build();
+    }
 }
